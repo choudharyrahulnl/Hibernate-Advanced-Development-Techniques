73cc1a0ecb1b0b32562adc8d0f72ebb563f66e81
choudharyrahulnl <choudhary.rahul.nl@gmail.com>
2022-12-28 21:25:36 +0100

    Initial Commit

---
+++ data/01-create-user.sql
@@ -0,0 +1,15 @@
+CREATE USER 'hbadvanceapp'@'localhost' IDENTIFIED BY 'hbadvanceapp';
+
+GRANT ALL PRIVILEGES ON * . * TO 'hbadvanceapp'@'localhost';
+
+#
+# Starting with MySQL 8.0.4, the MySQL team changed the 
+# default authentication plugin for MySQL server 
+# from mysql_native_password to caching_sha2_password.
+#
+# The command below will make the appropriate updates for your user account.
+#
+# See the MySQL Reference Manual for details: 
+# https://dev.mysql.com/doc/refman/8.0/en/caching-sha2-pluggable-authentication.html
+#
+ALTER USER 'hbadvanceapp'@'localhost' IDENTIFIED WITH mysql_native_password BY 'hbadvanceapp';
---
+++ src/main/java/com/spring/hibernateadvance/HibernateAdvanceApplication.java
@@ -0,0 +1,13 @@
+package com.spring.hibernateadvance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateAdvanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateAdvanceApplication.class, args);
+	}
+
+}
